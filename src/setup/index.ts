49d65bbf695f4b3f217a08b69c5afcbb4958e2c1
Kevin Malakoff <kmalakoff@gmail.com>
2026-01-02 20:06:46 -0800

    Upgrade keyv-registry

--- src/setup/index.ts
+++ src/setup/index.ts
@@ -1,5 +1,5 @@
 export { createConfig, parseConfig } from './config.ts';
 export { createHTTPServer } from './http.ts';
-export { createOAuthAdapters } from './oauth-google.ts';
+export { type AuthMiddleware, createOAuthAdapters, type OAuthAdapters, type OAuthRuntimeDeps } from './oauth-google.ts';
 export * from './runtime.ts';
 export { createStdioServer } from './stdio.ts';
